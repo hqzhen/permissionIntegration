01054e15969017c2f923e7d53d21613dfabc6f5c
zhenghongquan <zhenghongquan@banggood.com>
2022-08-22 11:49:15 +0800

    整合knife4j文档

--- src/main/java/com/zhq/permission/PermissionApplication.java
+++ src/main/java/com/zhq/permission/PermissionApplication.java
@@ -1,16 +1,24 @@
 package com.zhq.permission;
 
+import cn.hutool.core.net.NetUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 /**
  * @author zhenghongquan
  */
+@Slf4j
 @SpringBootApplication
 public class PermissionApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PermissionApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(PermissionApplication.class, args);
+        Environment environment = context.getBean(Environment.class);
+        log.info("启动完成->>>访问文档:http://{}:{}{}/doc.html", NetUtil.getLocalhost().getHostAddress(),
+                environment.getProperty("server.port"), environment.getProperty("server.servlet.context-path"));
     }
 
 }
