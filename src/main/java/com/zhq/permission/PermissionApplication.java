2f99dc5c8748c8ea09dff4ab642e2c7da235850f
zhenghongquan <zhenghongquan@banggood.com>
2022-08-22 11:34:29 +0800

    配置数据源

--- src/main/java/com/zhq/permission/PermissionApplication.java
+++ src/main/java/com/zhq/permission/PermissionApplication.java
@@ -3,6 +3,9 @@ package com.zhq.permission;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author zhenghongquan
+ */
 @SpringBootApplication
 public class PermissionApplication {
 
